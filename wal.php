3ec324e6eff02a802502844158c8bc29a9a0db6f
jhakardas <127119460+jhakardas@users.noreply.github.com>
2023-10-06 20:15:07 +0530

    Add files via upload

---
+++ wal.php
@@ -0,0 +1,7 @@
+<?php
+
+header('Location: https://www.walmart.com/ip/Avenco-10-inch-Gel-Memory-Foam-Pocketed-Springs-Hybrid-Mattress-in-a-Box-Pressure-Relief-Twin-Size-Mattress/460568700');
+
+exit;
+
+?>
